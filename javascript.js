44477e9a587f073e9a704eb5cbd80336ded08ac4
Leila Basri <leilabasrilb@gmail.com>
2023-11-27 15:24:12 -0800

    update

---
+++ javascript.js
@@ -0,0 +1,15 @@
+var number;
+function plusSlides(number){
+    var flick = document.getElementsByName("flickity-Image");
+    flick.length;
+}
+
+
+function nextSlide(){
+    plusSlides(1);
+}
+function prevSlide(){
+    plusSlides(-1);
+}
+function currentSlide(){
+}
